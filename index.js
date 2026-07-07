dbc9bd6e4d270f4b2463205f73755ae0d42fb315
Naweedhag <navidhaghighi2013@gmail.com>
2026-07-07 17:44:54 +0330

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function rollTheDice(){
+    const diceno=document.getElementById("dicenum").value;
+    const theresult = document.getElementById("theresult");
+    const theresult = document.getElementById("theimage");
+    const myvalue = [];
+    const imagevalue = [];
+
+    for(let i = 0; i<diceno ;i++){
+        const value = Math.floor(Math.random()*6) +1
+    }
+}
